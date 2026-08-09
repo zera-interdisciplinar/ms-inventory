4f43507a2c48123c6cf5c4f5f40081500de5f7ea
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2026-08-08 21:47:52 -0300

    feat: configure neo4j infrastructure dependency and connection properties

--- src/test/java/com/zera/ms_inventory/MsInventoryApplicationTests.java
+++ src/test/java/com/zera/ms_inventory/MsInventoryApplicationTests.java
@@ -2,8 +2,10 @@ package com.zera.ms_inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsInventoryApplicationTests {
 
     @Test
